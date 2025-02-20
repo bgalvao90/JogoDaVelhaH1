863fa0ab5a8452c02d531660da532559044dd459
048783 <048783@UNI-LAB2-35>
2025-02-19 21:42:16 -0300

    Adicionar arquivos de projeto.

---
+++ aula17-02/Program.cs
@@ -0,0 +1,57 @@
+﻿string[,] tabuleiro = new string[3,3];
+//matriz
+//3x3
+//armazenar X , O ou vazio
+
+int linha, coluna;
+string jogadorO = "O";
+string jogadorX = "X";
+string jogadorAtual = jogadorX;
+
+bool fimDeJogo = false;
+
+
+while (!fimDeJogo)
+{
+    Console.WriteLine("Jogador atual: "+jogadorAtual);
+    Console.WriteLine("Digite a linha:");
+    linha = int.Parse(Console.ReadLine())-1;
+
+    Console.WriteLine("Digite a coluna");
+    coluna = int.Parse(Console.ReadLine())-1;
+
+
+    if (tabuleiro[linha, coluna] == null)
+    {
+        tabuleiro[linha, coluna] = jogadorAtual;
+        if (jogadorAtual == jogadorX)
+            jogadorAtual = jogadorO;
+
+
+        else
+
+            jogadorAtual = jogadorX;
+    }
+    else
+    {
+        Console.WriteLine("Posição está ocupada.");
+        
+    }
+    ImprimirTabuleiro();
+}
+
+
+Console.ReadLine();
+
+
+void ImprimirTabuleiro()
+{
+    for (int linhaTabuleiro = 0; linhaTabuleiro < 3; linhaTabuleiro++)
+    {
+        for( int colunaTabuleiro = 0; colunaTabuleiro < 3; colunaTabuleiro++)
+        {
+            Console.Write(" " + tabuleiro[linhaTabuleiro,colunaTabuleiro] + " ");
+        }
+        Console.WriteLine();
+    }
+}
